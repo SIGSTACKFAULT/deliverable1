b78c34b827e7f0eb5cae843aca1ad1f9ec947e2c
Connor [REDACTED] <blacksilverck35@gmail.com>
2020-04-18 21:38:49 -0400

    add javadocs

--- src/war/WarRoundState.java
+++ src/war/WarRoundState.java
@@ -9,7 +9,10 @@ import cards.RegularCard;
 
 public class WarRoundState<C extends RegularCard> extends HashMap<WarPlayer<C>, LinkedList<C>> {
     public List<C> bounty = new LinkedList<C>();
-
+    /**
+     * tracks the state of a round of war.
+     * @param players list of players in the game
+     */
     public WarRoundState(List<WarPlayer<C>> players){
         super();
         for(WarPlayer<C> p : players){
@@ -17,6 +20,10 @@ public class WarRoundState<C extends RegularCard> extends HashMap<WarPlayer<C>, 
         }
     }
 
+    /**
+     * collect all the cards we're tracking and return them.
+     * @return
+     */
     public List<C> collectCards(){
         List<C> r = new LinkedList<C>();
         r.addAll(bounty);
@@ -26,16 +33,27 @@ public class WarRoundState<C extends RegularCard> extends HashMap<WarPlayer<C>, 
         return r;
     }
 
+    /**
+     * give all the cards we're tracking to p
+     * @param p player to give cards to
+     */
     public void giveCardsTo(WarPlayer<C> p){
         p.cards.addAll(collectCards());
     }
 
+    /**
+     * each player puts down a card.
+     */
     public void playCards(){
         for(WarPlayer<C> p : this.keySet()){
             this.playCard(p);
         }
     }
 
+    /**
+     * the given player puts down a card
+     * @param p player
+     */
     public void playCard(WarPlayer<C> p){
         this.get(p).add(p.takeCard());
     }
@@ -51,13 +69,17 @@ public class WarRoundState<C extends RegularCard> extends HashMap<WarPlayer<C>, 
     }
 
     /**
-     * @return the players currently tied for best card
+     * list the players tied for first place.
+     * may only return one, if they're the outright winner.
+     * @return
      */
     public List<WarPlayer<C>> detectWinners(){
-        List<WarPlayer<C>> winners = new LinkedList();
+        List<WarPlayer<C>> winners = new LinkedList<WarPlayer<C>>();
         int best = getBestValue();
-        for(WarPlayer<C> p : this.keySet()){
-
+        for(WarPlayer<C> p : keySet()){
+            if(getTopCard(p).getValue() == best){
+                winners.add(p);
+            }
         }
         return winners;
     }
@@ -74,13 +96,21 @@ public class WarRoundState<C extends RegularCard> extends HashMap<WarPlayer<C>, 
         return best;
     }
 
+    /**
+     * get the most-recently-played card by p
+     * @param p player
+     */
     public C getTopCard(WarPlayer<C> p){
         return this.get(p).peekLast();
     }
 
+    /**
+     * remove players whose top card isn't the best; put their cards in `bounty`
+     */
     public void eliminateLosers(){
         int best = getBestValue();
-        for(WarPlayer<C> p : this.keySet()){
+        List<WarPlayer<C>> players = new LinkedList<WarPlayer<C>>(this.keySet());
+        for(WarPlayer<C> p : players){
             if(getTopCard(p).getValue() < best){
                 bounty.addAll(remove(p));
             }
